82cc505e01fe77f9b3db7579669076c7b5d8068e
NikitaJTelisara <nikitatelisara@gmail.com>
2018-05-10 15:54:31 -0500

    Add files via upload

--- Point.java
+++ Point.java
@@ -1,9 +1,19 @@
-public class Point {
-    int x;
-    int y;
+package SDE_Agile;
 
-    public Point(int x,int y){
+public class Point implements Comparable<Point> {
+    double x;
+    double y;
+    double distanceFromR;
+
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point point) {
+        double d = ((Point) point).distanceFromR;
+        /* For Descending order*/
+        return (int) (d - this.distanceFromR);
     }
 }
