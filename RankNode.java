55d0815f56492f31c8fb13e96efa14f3d72610fc
NikitaJTelisara <nikitatelisara@gmail.com>
2019-04-06 22:48:26 -0500

    Update RankNode.java

--- RankNode.java
+++ RankNode.java
@@ -1,7 +1,8 @@
 public class RankNode {
-    int leftSize;
     RankNode left;
     RankNode right;
+    int nextGreaterNo;
+    int leftsize;
     int data;
 
     public RankNode(int data) {
@@ -15,11 +16,13 @@ public class RankNode {
             } else {
                 left.insert(d);
             }
-            leftSize++;
+            leftsize++;
         } else {
             if (right == null) {
                 right = new RankNode(d);
+                nextGreaterNo = d;
             } else {
+                nextGreaterNo = ((d < nextGreaterNo) ? d : nextGreaterNo);
                 right.insert(d);
             }
         }
@@ -27,22 +30,81 @@ public class RankNode {
 
     public int getRank(int d) {
         if (d == data) {
-            return leftSize + 1;
-        }
-        if (d < data) {
+            return leftsize + 1;
+        } else if (d < data) {
             return (left == null ? -1 : left.getRank(d));
         } else {
-            return (right == null ? -1 : (right.getRank(d) + 1 + leftSize));
+            return (right == null ? -1 : right.getRank(d) + leftsize + 1);
         }
     }
-    
+
+    public void print() {
+        if (left != null) {
+            left.print();
+        }
+        System.out.println(data);
+        if (right != null) {
+            right.print();
+        }
+    }
+
+
     /*Delete a node from a bst (https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/)
     1) Node to be deleted is leaf: Simply remove from the tree.
     2. Node to be deleted has only one child: Copy the child to the node and delete the child
-    3. Node to be deleted has two children: 
-       Find inorder successor of the node. Copy contents of the inorder 
+    3. Node to be deleted has two children:
+       Find inorder successor of the node. Copy contents of the inorder
        successor to the node and delete the inorder successor. Note that inorder predecessor can also be used.
-       
-       
+
     */
+    public RankNode remove(int d) {
+        if (d == data) {
+            if (isleaf()) {
+                return null;
+            } else if (left == null || right == null) {
+                if (left == null) {
+                    return right;
+                } else if (right == null) {
+                    return left;
+                }
+            } else if (left != null && right != null) {
+                int n = getNextGreaterNo(nextGreaterNo);
+                this.remove(nextGreaterNo);
+                data = nextGreaterNo;
+                nextGreaterNo = n;
+                return this;
+            }
+        } else if (d < data) {
+            left = (left == null ? null : left.remove(d));
+        } else if (d > data) {
+            right = (right == null ? null : right.remove(d));
+        }
+        return this;
+    }
+
+    public int getNextGreaterNo(int d){
+        if(d== data){
+            return nextGreaterNo;
+        } if(d<data){
+            return left.getNextGreaterNo(d);
+        }  else{
+            return right.getNextGreaterNo(d);
+        }
+    }
+
+    public boolean isleaf(){
+        return (left == null && right == null);
+    }
+
+    public boolean noleft(){
+        return (left == null && right != null);
+    }
+
+    public boolean noright(){
+        return (left == null && right != null);
+    }
+
 }
+
+
+
